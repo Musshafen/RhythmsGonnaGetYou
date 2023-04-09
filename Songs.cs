15890c5fb77050da554a5e05c89f2f2176838359
KM Musshafen <kmusshafen@hotmail.com>
2023-04-08 21:15:33 -0400

    Created Songs class

---
+++ Songs.cs
@@ -0,0 +1,13 @@
+namespace RhythmsGonnaGetYou
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public int AlbumId { get; set; }
+
+
+    }
+}
