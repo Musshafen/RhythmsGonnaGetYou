71e1e2692662cb4a5f96e6939b1124720246f446
KM Musshafen <kmusshafen@hotmail.com>
2023-04-11 19:08:13 -0400

    Creating Menu Add Band and BAnd Criteria

--- Program.cs
+++ Program.cs
@@ -5,9 +5,46 @@ namespace RhythmsGonnaGetYou
     class Program
     {
         static void Main(string[] args)
+
+
+
+
+
         {
+
             Console.WriteLine("Hi there! Welcome to the Record Company Database");
 
+            var quit = false;
+
+            while (quit == false)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Would you like to [A]dd a new band?: ");
+
+
+
+
+
+
+
+                var choice = Console.ReadLine().ToUpper();
+                if (choice == "A")
+                {
+                    Console.WriteLine("What is the new band's name?");
+                    var name = Console.ReadLine();
+
+
+                }
+
+
+
+
+            }
+
+
+
+
+        }
 
 
 
